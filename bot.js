0fe56552d2a0c505ce5a83da384da56e31162854
Ubuntu <ubuntu@hackthon.infra.cloudnative.tw>
2022-10-22 19:02:37 +0000

    init

---
+++ bot.js
@@ -0,0 +1,36 @@
+const { driver } = require('@rocket.chat/sdk');
+//const respmap  = require('./reply');
+
+// Bot configuration
+const runbot = async () => {
+    const conn = await driver.connect({ host: HOST, useSsl: SSL })
+    myUserId = await driver.login({ username: USER, password: PASS });
+    const roomsJoined = await driver.joinRooms( ROOMS );
+    console.log('joined rooms');
+
+    const subscribed = await driver.subscribeToMessages();
+    console.log('subscribed');
+
+    const msgloop = await driver.reactToMessages( processMessages );
+    console.log('connected and waiting for messages');
+
+    const sent = await driver.sendToRoom( BOTNAME + ' is listening ...', ROOMS[0]);
+    console.log('Greeting message sent');
+}
+
+// Process messages
+const processMessages = async(err, message, messageOptions) => {
+if (!err) {
+    if (message.u._id === myUserId) return;
+    const roomname = await driver.getRoomName(message.rid);
+
+    console.log('got message ' + message.msg)
+    var response;
+    if (message.msg in respmap) {
+        response = respmap[message.msg];
+    }
+    const sentmsg = await driver.sendToRoomId(response, message.rid)
+    }
+}
+
+runbot()
